621ec8e83565fd1998e0cd0af96a2f17f9829fc2
Hari-web444 <harirv.krish@gmail.com>
2025-05-22 16:21:17 +0530

    branch api

--- src/controller/branchcontroller.js
+++ src/controller/branchcontroller.js
@@ -61,7 +61,6 @@ exports.getLastBranchId = async (req, res) => {
     const { state, location } = req.body;
 
     const sqlBranch = `CALL SP_GetLastBranchId('${state}', '${location}')`;
-    const sqlHead = `CALL SP_GetBranchHead()`;
 
     try {
         db.query(sqlBranch, (err, branchResult) => {
@@ -70,16 +69,28 @@ exports.getLastBranchId = async (req, res) => {
                 return res.status(500).json({ message: 'Failed to get branch ID list' });
             }
 
-            db.query(sqlHead, (err, headResult) => {
-                if (err) {
-                    console.error('Error getting branch head:', err);
-                    return res.status(500).json({ message: 'Failed to get branch head' });
-                }
+            res.status(200).json({
+                branchData: branchResult[0]
+            });
+        });
+    } catch (error) {
+        console.error('Server error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
 
-                res.status(200).json({
-                    branchData: branchResult[0],
-                    headData: headResult[0]
-                });
+exports.getBranchHeadList = async (req, res) => {
+    const sqlHead = `CALL SP_GetBranchHead()`;
+
+    try {
+        db.query(sqlHead, (err, headResult) => {
+            if (err) {
+                console.error('Error getting branch head:', err);
+                return res.status(500).json({ message: 'Failed to get branch head' });
+            }
+
+            res.status(200).json({
+                headData: headResult[0]
             });
         });
     } catch (error) {
@@ -95,6 +106,7 @@ exports.saveBranchDetails = async (req, res) => {
         assignBrandasVaithyar,
         branch_id,
         branch_in_charge,
+        branch_incharge_recid,
         branch_name,
         branch_type,
         country,
@@ -107,7 +119,7 @@ exports.saveBranchDetails = async (req, res) => {
         state
     } = req.body;
 
-    const sql = `CALL SP_SaveBranchDetails(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const sql = `CALL SP_SaveBranchDetails(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
     try {
         db.query(
@@ -117,6 +129,7 @@ exports.saveBranchDetails = async (req, res) => {
                 branch_name,
                 branch_type,
                 branch_in_charge,
+                branch_incharge_recid,
                 email,
                 phone_number,
                 address,
@@ -141,6 +154,80 @@ exports.saveBranchDetails = async (req, res) => {
                 });
             }
         );
+    } catch (error) {
+        console.error('Server error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+exports.getBranchDetails = async (req, res) => {
+    const { userId, user_typecode } = req.body;
+
+    const sqlBranch = `CALL SP_GetBranchDetails(${userId}, '${user_typecode}')`;
+
+    try {
+        db.query(sqlBranch, (err, branchResult) => {
+            if (err) {
+                console.error('Error getting branch:', err);
+                return res.status(500).json({ message: 'Failed to get branch ID list' });
+            }
+
+            res.status(200).json({
+                data: branchResult[0],
+            });
+        });
+    } catch (error) {
+        console.error('Server error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+exports.updateBranchDetails = async (req, res) => {
+    try {
+        const { updates } = req.body;
+
+        if (!Array.isArray(updates) || updates.length === 0) {
+            return res.status(400).json({ message: 'No updates provided.' });
+        }
+
+        const branch_id = updates[0].branch_id;
+
+        const query = (sql, values) =>
+            new Promise((resolve, reject) => {
+                db.query(sql, values, (err, result) => {
+                    if (err) return reject(err);
+                    resolve(result);
+                });
+            });
+
+        for (const { key, newValue } of updates) {
+            const sql = `UPDATE branches SET ${key} = ? WHERE branch_id = ?`;
+            await query(sql, [newValue, branch_id]);
+        }
+
+        res.status(200).json({ message: 'Employee updated successfully.' });
+    } catch (err) {
+        console.error('Error updating employee:', err);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+};
+
+exports.deleteSelBranchList = async (req, res) => {
+    const { branch_id } = req.body;
+
+    const sqlBranch = `CALL SP_DelSelBranchList('${branch_id}')`;
+
+    try {
+        db.query(sqlBranch, (err, branchResult) => {
+            if (err) {
+                console.error('Error getting branch:', err);
+                return res.status(500).json({ message: 'Failed to get branch ID list' });
+            }
+
+            res.status(200).json({
+                data: branchResult[0],
+            });
+        });
     } catch (error) {
         console.error('Server error:', error);
         res.status(500).json({ message: 'Server error' });
--- src/routes/branchroute.js
+++ src/routes/branchroute.js
@@ -8,5 +8,9 @@ router.post('/getStateByCoutry', brcontroller.getStateByCoutry);
 router.post('/getCityByState', brcontroller.getCityByState);
 router.post('/getLastBranchId', brcontroller.getLastBranchId);
 router.post('/saveBranchDetails', brcontroller.saveBranchDetails);
+router.post('/getBranchDetails', brcontroller.getBranchDetails);
+router.get('/getBranchHeadList', brcontroller.getBranchHeadList);
+router.post('/updateBranchDetails', brcontroller.updateBranchDetails);
+router.post('/deleteSelBranchList', brcontroller.deleteSelBranchList);
 
 module.exports = router;
